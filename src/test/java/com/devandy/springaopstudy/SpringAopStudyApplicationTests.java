08b32ad3fa295e0dfb1fcb05f8dc4e2748db87d3
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-05-21 17:46:24 +0900

    init first

---
+++ src/test/java/com/devandy/springaopstudy/SpringAopStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devandy.springaopstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
